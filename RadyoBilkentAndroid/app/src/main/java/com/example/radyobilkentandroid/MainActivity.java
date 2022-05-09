8ef345b9a3130db4036c31b3ca0d65757a2615a0
Ali Cevat Ercal <cevat.ercal@ug.bilkent.edu.tr>
2022-05-09 12:06:53 +0300

    updated MainActivity

--- RadyoBilkentAndroid/app/src/main/java/com/example/radyobilkentandroid/MainActivity.java
+++ RadyoBilkentAndroid/app/src/main/java/com/example/radyobilkentandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.radyobilkentandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -19,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        NowPlayingFragment nowPlayingFragment = NowPlayingFragment.newInstance(url, "Never Gonna Give You Up \n Rick Astley");
+        ft.replace(R.id.nowPlayingFragment, nowPlayingFragment);
+        ft.commit();
     }
 }
